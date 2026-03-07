0b25228101e17caf4ba0693c8439eebca8b355cd
abdullhaks <aks4u786@gmail.com>
2026-03-07 14:55:14 +0530

    refact: 2

--- BACKEND/src/shared/notification.service.ts
+++ BACKEND/src/shared/notification.service.ts
@@ -31,8 +31,8 @@ export class NotificationService {
     ) {
         this.transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
-            port: 465,
-            secure: true, // true for 465, false for 587
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
